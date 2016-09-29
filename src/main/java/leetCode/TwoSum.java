2c30585d3cfcdf5ce66b50bbbeb5fc1990896c14
Dave <1092626412@qq.com>
2016-09-30 00:27:41 +0800

    提交leetCode

--- src/main/java/leetCode/TwoSum.java
+++ src/main/java/leetCode/TwoSum.java
@@ -8,13 +8,21 @@ public class TwoSum {
     public static void main(String[] args){
         int[] a= {34,56,2,1};
         int target = 3;
-        for(int i = 0; i < a.length; i ++){
-            for(int j = i + 1; j < a.length; j ++){
-                int sum = a[i] + a[j];
+        TwoSum example = new TwoSum();
+        example.twoSum(a, target);
+    }
+    public int[] twoSum(int[] nums, int target){
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
                 if(sum == target){
-                    System.out.println("a[" + i + "]+" + "a[" + j + "] = " + target);
+                    result[0] = i + 1;
+                    result[1] = j + 1;
                 }
             }
+
         }
+        return result;
     }
 }
