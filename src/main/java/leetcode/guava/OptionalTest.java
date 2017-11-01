7a96b3b2346fdde8bcdb84a684c64d89e13a8394
Dave <kingdavehangzhou@gmail.com>
2017-11-01 22:29:56 +0800

    optional

--- src/main/java/leetcode/guava/OptionalTest.java
+++ src/main/java/leetcode/guava/OptionalTest.java
@@ -13,10 +13,38 @@ import java.util.Optional;
 public class OptionalTest {
 
     @Test
-    public void testOptional(){
+    public void testOptional() {
         Optional<Integer> possible = Optional.of(6);
-        if (possible.isPresent()){
+        if (possible.isPresent()) {
             Assert.assertEquals(Optional.of(6), Optional.of(possible.get()));
         }
+    }
+
+    @Test
+    public void testOptional2() {
+        Optional<Integer> possible = Optional.of(6);
+        Optional<String> nullPossible = Optional.ofNullable(null);
+
+        Assert.assertEquals(true, possible.isPresent());
+        Assert.assertEquals(Optional.of(6), possible);
+    }
+
+    private Optional method() {
+        return Optional.ofNullable(null);
+    }
+
+    private Optional methodNoNull() {
+        return Optional.ofNullable(15L);
+    }
+
+    public void testMethodReturn() {
+        Optional<Long> value = method();
+        if (value.isPresent()){
+            System.out.println(value.get());
+        } else {
+            System.out.println(value.orElse(-12L));
+        }
+        System.out.println(value.get());
+//        value.ifPresent();
     }
 }
