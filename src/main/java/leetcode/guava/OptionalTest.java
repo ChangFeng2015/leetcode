41456517a1cfdbb0326ef513b318f2e29da1fc62
Dave <kingdavehangzhou@gmail.com>
2017-11-07 17:58:11 +0800

    List iterator

--- src/main/java/leetcode/guava/OptionalTest.java
+++ src/main/java/leetcode/guava/OptionalTest.java
@@ -1,9 +1,11 @@
 package leetcode.guava;
 
+import com.google.common.base.Optional;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Optional;
+
+
 
 /**
  * guava optional
@@ -12,39 +14,56 @@ import java.util.Optional;
  */
 public class OptionalTest {
 
-    @Test
-    public void testOptional() {
-        Optional<Integer> possible = Optional.of(6);
-        if (possible.isPresent()) {
-            Assert.assertEquals(Optional.of(6), Optional.of(possible.get()));
-        }
-    }
+//    @Test
+//    public void testOptional() {
+//        Optional<Integer> possible = Optional.of(6);
+//        if (possible.isPresent()) {
+//            Assert.assertEquals(Optional.of(6), Optional.of(possible.get()));
+//        }
+//    }
+//
+//    @Test
+//    public void testOptional2() {
+//        Optional<Integer> possible = Optional.of(6);
+//        Optional<String> nullPossible = Optional.ofNullable(null);
+//
+//        Assert.assertEquals(true, possible.isPresent());
+//        Assert.assertEquals(Optional.of(6), possible);
+//    }
+//
+//    private Optional method() {
+//        return Optional.ofNullable(null);
+//    }
+//
+//    private Optional methodNoNull() {
+//        return Optional.ofNullable(15L);
+//    }
+//
+//    public void testMethodReturn() {
+//        Optional<Long> value = method();
+//        if (value.isPresent()){
+//            System.out.println(value.get());
+//        } else {
+//            System.out.println(value.orElse(-12L));
+//        }
+//        System.out.println(value.get());
+////        value.ifPresent();
+//    }
 
     @Test
-    public void testOptional2() {
-        Optional<Integer> possible = Optional.of(6);
-        Optional<String> nullPossible = Optional.ofNullable(null);
-
-        Assert.assertEquals(true, possible.isPresent());
-        Assert.assertEquals(Optional.of(6), possible);
+    public void test() {
+        MoneyAccount moneyAccount = new MoneyAccount();
+        OptionalTest(null);
     }
 
-    private Optional method() {
-        return Optional.ofNullable(null);
-    }
-
-    private Optional methodNoNull() {
-        return Optional.ofNullable(15L);
-    }
-
-    public void testMethodReturn() {
-        Optional<Long> value = method();
-        if (value.isPresent()){
-            System.out.println(value.get());
-        } else {
-            System.out.println(value.orElse(-12L));
-        }
-        System.out.println(value.get());
-//        value.ifPresent();
+    private void OptionalTest(MoneyAccount moneyAccount) {
+        MoneyAccount instance = new MoneyAccount();
+        Optional<MoneyAccount> moneyAccountOptional = com.google.common.base.Optional.fromNullable(moneyAccount);
+//        if (moneyAccountOptional.isPresent()) {
+            instance.setCode(moneyAccountOptional.get().getCode());
+            instance.setId(moneyAccountOptional.get().getId());
+            instance.setName(moneyAccountOptional.get().getName());
+//        }
+        Assert.assertSame(moneyAccount, instance);
     }
 }
