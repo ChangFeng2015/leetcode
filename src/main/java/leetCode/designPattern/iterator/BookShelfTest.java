0895c69266d00fe1bbf65e27b3efab88613ed056
Dave <kingdavehangzhou@gmail.com>
2018-10-05 18:17:45 +0800

    iterator and adaptor demo

--- src/main/java/leetCode/designPattern/iterator/BookShelfTest.java
+++ src/main/java/leetCode/designPattern/iterator/BookShelfTest.java
@@ -1,26 +1,26 @@
 package leetCode.designPattern.iterator;
 
+import leetCode.designPattern.Adaptor.FileIO;
+import leetCode.designPattern.Adaptor.FileProperties;
 import org.junit.Test;
 
-/**
- * @ClassName BookShelfTest
- * @Description TODO
- * @Author Dave
- * @Date 2018/9/23 12:02
- * @Version 1.0
- **/
-public class BookShelfTest {
+import java.io.IOException;
 
+public class BookShelfTest {
     @Test
-    public void shelfTest() {
-        BookShelf bookShelf = new BookShelf(3);
-        bookShelf.appendBook(new Book("book A"));
-        bookShelf.appendBook(new Book("book B"));
-        bookShelf.appendBook(new Book("book C"));
-        bookShelf.appendBook(new Book("book d"));
+    public void test() {
+        BookShelf bookShelf = new BookShelf();
+        for (int i = 0; i < 10; i ++) {
+            Book bookOne = new Book("book " + i);
+            bookShelf.appeand(bookOne);
+        }
+
         Iterator<Book> iterator = bookShelf.iterator();
         while (iterator.hasNext()) {
-            System.out.println(iterator.next().getName());
+            Book book = iterator.next();
+            System.out.println(book.getName());
         }
+
     }
+
 }
--- src/main/java/leetCode/designPattern/iterator/Iterator.java
+++ src/main/java/leetCode/designPattern/iterator/Iterator.java
@@ -1,13 +1,6 @@
 package leetCode.designPattern.iterator;
 
-/**
- * @ClassName Iterator
- * @Description TODO
- * @Author Dave
- * @Date 2018/9/23 11:51
- * @Version 1.0
- **/
-public interface Iterator<E> {
-    E next();
+public interface Iterator <T> {
     boolean hasNext();
+    T next();
 }
